ccf6cfad12add7b73b2ad4fbb23f677c4ebc4f42
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2023-09-05 12:21:07 +0200

    Fix imports

--- jellyfish/__init__.py
+++ jellyfish/__init__.py
@@ -6,8 +6,8 @@ __version__ = "0.1.0"
 
 from pydantic.error_wrappers import ValidationError  # pylint: disable=no-name-in-module
 
-from jellyfish_openapi_client import Room, RoomConfig, Peer, Component
-from jellyfish_openapi_client import ComponentOptionsRTSP
-from jellyfish_openapi_client import PeerOptionsWebRTC
-
 from jellyfish.room_api import RoomApi
+
+from .openapi_client.jellyfish_openapi_client import Room, RoomConfig, Peer, Component
+from .openapi_client.jellyfish_openapi_client import ComponentOptionsRTSP
+from .openapi_client.jellyfish_openapi_client import PeerOptionsWebRTC
