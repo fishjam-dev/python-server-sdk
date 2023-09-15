9c0df2a7db35b250e4008e7a1c67769cc918d0f8
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2023-09-15 13:14:23 +0200

    docs title

---
+++ pdoc.sh
@@ -0,0 +1,6 @@
+pdoc\
+    --favicon https://logo.swmansion.com/membrane/\?width\=100\&variant\=signetDark\
+    --logo https://logo.swmansion.com/membrane/\?width\=70\&variant\=signetDark\
+    -t doc_templates \
+    -o doc \
+    jellyfish
