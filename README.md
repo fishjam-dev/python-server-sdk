af70bf36eca6e6109696d8395d673f08b75395c9
Przemysław Rożnawski <48837433+roznawsk@users.noreply.github.com>
2023-09-18 10:00:29 +0200

    Add docs (#4)
    * Update readme
    
    * generate docs
    
    * github pages
    
    * Fix tests
    
    * fix typo
    
    * Fix
    
    * Fix lint
    
    * impove naming
    
    * Cleanup ci job
    
    * switch to main branch
    
    * docs title
    
    * Default server address and token
    
    * Add docstrings
    
    * Switch to main
    
    * Update jellyfish/_room_api.py
    
    Co-authored-by: Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jakub Pisarek <99591440+sgfn@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,12 +2,44 @@
 
 [![CircleCI](https://dl.circleci.com/status-badge/img/gh/jellyfish-dev/python-server-sdk/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/jellyfish-dev/python-server-sdk/tree/main)
 
-Python server SDK for [Jellyfish](https://github.com/jellyfish-dev/jellyfish).
+Python server SDK for the [Jellyfish](https://github.com/jellyfish-dev/jellyfish) media server.
+
+Read the docs [here](https://jellyfish-dev.github.io/python-server-sdk/jellyfish.html)
 
 ## Installation
-TODO
+
+You can install the latest version of the package from github:
+```
+pip install git+https://github.com/jellyfish-dev/python-server-sdk
+```
 
 ## Usage
+
+First create a `RoomApi` instance, providing the jellyfish server address and api token
+
+```python
+from jellyfish import RoomApi
+
+room_api = RoomApi(server_address='localhost:5002', server_api_token='development')
+```
+
+You can use it to interact with Jellyfish managing rooms, peers and components
+
+```python
+# Create a room
+jellyfish_address, room = room_api.create_room()
+# 'localhost:5002', Room(components=[], id='f7cc2eac-f699-4609-ac8f-92f1ad6bea0c', peers=[])
+
+# Add peer to the room
+from jellyfish import PeerOptionsWebRTC
+
+peer_token, peer_webrtc = room_api.add_peer(room.id, peer_type='webrtc', options=PeerOptionsWebRTC())
+# 'AgDYfrCSigFiAA', Peer(id='2869fb5', status=<PeerStatus.DISCONNECTED: 'disconnected'>, type='webrtc')
+
+# Add component to the room
+component_hls = room_api.add_component(room.id, component_type='hls')
+# Component(id='4c028a86', metadata=ComponentMetadata(playable=False), type='hls')
+```
 
 ## Copyright and License
 
--- jellyfish/_room_api.py
+++ jellyfish/_room_api.py
@@ -5,13 +5,17 @@
 from jellyfish import _openapi_client as jellyfish_api
 
 from jellyfish._openapi_client import (AddPeerRequest, AddComponentRequest, PeerOptions,
-                            ComponentOptions, Room, RoomConfig, Peer, Component)
+                                       ComponentOptions, Room, RoomConfig, Peer, Component)
 
 
 class RoomApi:
-    """Allows for managing rooms"""
+    '''Allows for managing rooms'''
 
-    def __init__(self, server_address: str, server_api_token: str):
+    def __init__(self,
+                 server_address: str = 'localhost:5002', server_api_token: str = 'development'):
+        '''
+        Create RoomApi instance, providing the jellyfish address and api token.
+        '''
         self._configuration = jellyfish_api.Configuration(
             host=server_address,
             access_token=server_api_token
@@ -21,30 +25,41 @@ class RoomApi:
         self._room_api = jellyfish_api.RoomApi(self._api_client)
 
     def create_room(self, max_peers: int = None, video_codec: str = None) -> (str, Room):
-        """Creates a room"""
+        '''
+        Creates a new room
 
+        Returns a tuple (`jellyfish_address`, `Room`) - the address of the Jellyfish
+        in which the room has been created and the created `Room`
+        '''
         room_config = RoomConfig(maxPeers=max_peers, videoCodec=video_codec)
         resp = self._room_api.create_room(room_config)
 
         return (resp.data.jellyfish_address, resp.data.room)
 
     def delete_room(self, room_id: str) -> None:
-        """Deletes a room"""
+        '''Deletes a room'''
 
         return self._room_api.delete_room(room_id)
 
     def get_all_rooms(self) -> list:
-        """Returns list of all rooms"""
+        '''Returns list of all rooms '''
 
         return self._room_api.get_all_rooms().data
 
     def get_room(self, room_id: str) -> Room:
-        """Returns room with the given id"""
+        '''Returns room with the given id'''
 
         return self._room_api.get_room(room_id).data
 
     def add_peer(self, room_id: str, peer_type: str, options) -> (str, Peer):
-        """Creates peer in the room"""
+        '''
+        Creates peer in the room
+
+        Currently only `'webrtc'` peer is supported
+
+        Returns a tuple (`peer_token`, `Peer`) - the token needed by Peer to authenticate
+        to Jellyfish and the new `Peer`        
+        '''
 
         options = PeerOptions(options)
         request = AddPeerRequest(type=peer_type, options=options)
@@ -53,12 +68,12 @@ class RoomApi:
         return (resp.data.token, resp.data.peer)
 
     def delete_peer(self, room_id: str, peer_id: str) -> None:
-        """Deletes peer"""
+        '''Deletes peer'''
 
         return self._room_api.delete_peer(room_id, peer_id)
 
     def add_component(self, room_id: str, component_type: str, options=None) -> Component:
-        """Creates component in the room"""
+        '''Creates component in the room'''
 
         if options is not None:
             options = ComponentOptions(options)
@@ -68,6 +83,6 @@ class RoomApi:
         return self._room_api.add_component(room_id, request).data
 
     def delete_component(self, room_id: str, component_id: str) -> None:
-        """Deletes component"""
+        '''Deletes component'''
 
         return self._room_api.delete_component(room_id, component_id)
