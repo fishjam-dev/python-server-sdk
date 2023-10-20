62c84a319225008f49e841dfb2e4f3bf35f07cde
Radosław Szuma <radoslawszuma@gmail.com>
2023-10-20 14:36:41 +0200

    Add step for checking formatting to CI

--- test_command.py
+++ test_command.py
@@ -6,8 +6,13 @@ def run_tests():
     os.system("docker compose -f docker-compose-test.yaml run --remove-orphans test")
 
 
+def check_format():
+    os.system("black --check .")
+
+
 def run_formatter():
     os.system("black .")
+
 
 def run_linter():
     os.system("poetry run pylint --rcfile=pylintrc jellyfish tests")
