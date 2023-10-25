8d30e04325e0b5abd4fabe1ca7367c45a450bf24
Radosław Szuma <radoslawszuma@gmail.com>
2023-10-25 11:06:39 +0200

    Return to static IP in docker-compose

--- test_commands.py
+++ test_commands.py
@@ -3,7 +3,7 @@ import os
 def run_tests():
     os.system("docker rm -f jellyfish")
     os.system("docker compose -f docker-compose-test.yaml pull")
-    os.system("docker compose -f docker-compose-test.yaml up --remove-orphans test")
+    os.system("docker compose -f docker-compose-test.yaml run --remove-orphans test")
     os.system("docker compose -f docker-compose-test.yaml down")
 
 
